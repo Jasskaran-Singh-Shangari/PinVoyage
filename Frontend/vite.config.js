c18b273e07dd8cfbbe23606d671deb248f95022d
Jasskaran Singh Shangari <jasskaransingh0007@gmail.com>
2025-05-11 18:43:48 +0530

    Solving chunking problem

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit from 500 KB to 1MB (optional)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Extract each dependency into its own chunk
+            return id.toString().split('node_modules/')[1].split('/')[0];
+          }
+        }
+      }
+    }
+  }
+});
